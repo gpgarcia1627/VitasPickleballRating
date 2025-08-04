63f877d4b7ed487f694fe13267f5b66881a45c12
Gerardo Garcia <ggarcia1@vitas.com>
2025-08-03 23:25:44 -0400

    Add readme and some fixups

---
+++ README.md
@@ -0,0 +1,13 @@
+# VitasPickleballRating
+Pickleball Rating system for use by Vitas players.
+1. It keeps track of players, teams and games played.
+1. It estimates the score of a game between two teams.
+1. It computes a new rating for each player after a game.
+1. It displays win-loss standings of player and their rating..
+1. It displays a players rating over time.
+
+## Getting Started
+### Database Schema
+The Schema is shown [Here](./VPR_Schema.png)
+
+## Building
--- vpr/Tables/PlayerRating.sql
+++ vpr/Tables/PlayerRating.sql
@@ -4,6 +4,8 @@
     [PlayerId]          INT         NOT NULL,
     [GameId]            INT         NULL,
     [Rating]            INT         NOT NULL,
-    [RatingDate]        DATETIMEOFFSET NOT NULL DEFAULT SYSDATETIMEOFFSET()
+    [RatingDate]        DATETIMEOFFSET NOT NULL DEFAULT SYSDATETIMEOFFSET(), 
+    CONSTRAINT [FK_PlayerRating_Player] FOREIGN KEY ([PlayerId]) REFERENCES [Player]([PlayerId]), 
+    CONSTRAINT [FK_PlayerRating_game] FOREIGN KEY ([GameId]) REFERENCES [Game]([GameId])
 
 )
